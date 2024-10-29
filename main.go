dd4d0f29255ca3a7d5f988176161f779391f8e40
Zefanya211004 <zefanyatfs21@gmail.com>
2024-10-30 03:55:17 +0800

    Update main.go

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ func main() {
 	conferenceName := "Go Conference"
 	const conferenceTickets int = 50
 	var remainingTickets uint = 50
-	var bookings [50]string
+	var bookings []string
 
 	fmt.Printf("Welcome to %v booking application\n", conferenceName)
 	fmt.Printf("We have total of %v tickets and %v are still avalailable\n", conferenceTickets, remainingTickets)
@@ -30,12 +30,12 @@ func main() {
 	fmt.Scan(&userTickets)
 
 	remainingTickets = remainingTickets - userTickets
-	bookings[0] = firstName + " " + lastName
+	bookings = append(bookings, firstName+" "+lastName)
 
-	fmt.Printf("The whole array : %v\n", bookings)
+	fmt.Printf("The whole slice : %v\n", bookings)
 	fmt.Printf("The first value : %v\n", bookings[0])
-	fmt.Printf("Array type : %T\n", bookings)
-	fmt.Printf("Array length : %v\n", len(bookings))
+	fmt.Printf("Slice type : %T\n", bookings)
+	fmt.Printf("Slice length : %v\n", len(bookings))
 
 	fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v\n", firstName, lastName, userTickets, email)
 	fmt.Printf("%v tickets remaining for %v\n", remainingTickets, conferenceName)
